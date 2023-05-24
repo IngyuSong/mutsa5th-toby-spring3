7cc6895421afce79969ba891571ae64bb98c71ce
Ingyu Song <ingyu3879@gmail.com>
2023-05-24 14:28:56 +0900

    feat: SimpleConnectionMaker 적용

--- src/main/java/com/example/tobyspring3/db/dao/UserDao.java
+++ src/main/java/com/example/tobyspring3/db/dao/UserDao.java
@@ -7,20 +7,12 @@ import java.util.Map;
 
 import static java.lang.System.getenv;
 
-public abstract class UserDao {
+public class UserDao {
 
-    public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
-//    {
-//        Map<String, String> env = getenv();
-//        String dbHost = env.get("DB_HOST");
-//        String dbUser = env.get("DB_USER");
-//        String dbPassword = env.get("DB_PASSWORD");
-//        Class.forName("com.mysql.cj.jdbc.Driver");
-//        Connection conn = DriverManager.getConnection(dbHost, dbUser, dbPassword);
-//        return conn;
-//}
+    SimpleConnectionMaker connectionMaker = new SimpleConnectionMaker();
+
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Connection conn = getConnection();
+        Connection conn = connectionMaker.makeNewConnection();
 
         PreparedStatement pstmt = conn.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
         pstmt.setString(1, user.getId());
@@ -32,7 +24,7 @@ public abstract class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection conn = getConnection();
+        Connection conn = connectionMaker.makeNewConnection();
 
         PreparedStatement pstmt = conn.prepareStatement("select id, name, password from users where id = ?");
         pstmt.setString(1, id);
